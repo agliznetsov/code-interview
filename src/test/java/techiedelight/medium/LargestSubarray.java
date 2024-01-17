f6bcb32609e4ee84d4da5d3b434f1c309789bbac
Andrey Gliznetsov <a.gliznetsov@gmail.com>
2024-01-17 16:25:08 +0100

    MergeArrays

---
+++ src/test/java/techiedelight/medium/LargestSubarray.java
@@ -0,0 +1,72 @@
+package techiedelight.medium;
+/*
+
+Given a binary array containing 0’s and 1’s, find the largest contiguous subarray with equal numbers of 0’s and 1’s.
+
+Input : [0, 0, 1, 0, 1, 0, 0]
+Output: [0, 1, 0, 1] or [1, 0, 1, 0]
+
+Input : [0, 0, 0, 0]
+Output: []
+
+Note: Since an input can contain several largest subarrays with equal numbers of 0’s and 1’s, the code should return any one of them.
+
+*/
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+class LargestSubarray {
+
+	public static List<Integer> findLargestSubarray(List<Integer> nums) {
+	    Map<Integer, Integer> map = new HashMap<>();
+
+	    // insert (0, -1) pair into the set to handle the case when a
+	    // subarray with sum `S` starts from index 0
+	    map.put(0, -1);
+
+	    int target = 0;
+
+	    // `len` stores the maximum length of subarray with sum `S`
+	    int len = 0;
+
+	    // stores ending index of the maximum length subarray having sum `S`
+	    int ending_index = -1;
+
+	    // traverse the given array
+	    for (int i = 0; i < nums.size(); i++)
+	    {
+		    // sum of elements so far
+		    int num = nums.get(i);
+		    target += num == 0 ? -1 : 1;
+
+		    // if the sum is seen for the first time, insert the sum with its
+		    // into the map
+		    map.putIfAbsent(target, i);
+
+		    // update length and ending index of the maximum length subarray
+		    // having sum `S`
+		    Integer start = map.get(target);
+		    if (start != null && len < i - start)
+		    {
+			    len = i - start;
+			    ending_index = i;
+		    }
+	    }
+
+	    return nums.subList(ending_index - len + 1, ending_index + 1);
+    }
+
+    @Test
+    void test() {
+        assertEquals(List.of(0, 1, 0, 1),
+		        findLargestSubarray(List.of(0, 0, 1, 0, 1, 0, 0)));
+    }
+
+
+}
---
+++ src/test/java/techiedelight/medium/MergeArrays.java
@@ -0,0 +1,48 @@
+package techiedelight.medium;
+/*
+
+Given two sorted integer arrays, `X[]` and `Y[]` of size `m` and `n` each, in-place merge elements of `X[]` with elements of array `Y[]` by maintaining the sorted order, i.e., fill `X[]` with the first `m` smallest elements and fill `Y[]` with remaining elements.
+
+Input :
+
+X[] = [1, 4, 7, 8, 10]
+Y[] = [2, 3, 9]
+
+Output:
+
+X[] = [1, 2, 3, 4, 7]
+Y[] = [8, 9, 10]
+
+*/
+
+import static techiedelight.ArrayUtils.swap;
+import static techiedelight.ArrayUtils.toList;
+
+import org.junit.jupiter.api.Test;
+
+class MergeArrays {
+    public static void merge(int[] x, int[] y) {
+        for (int i = 0; i < x.length; i++) {
+            if (x[i] > y[0]) {
+                int tmp = x[i];
+                x[i] = y[0];
+                y[0] = tmp;
+
+                for (int n = 1; n < y.length; n++) {
+                    if (y[n - 1] > y[n]) {
+                        swap(y, n - 1, n);
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    void test() {
+        int[] x = new int[] {1, 4, 7, 8, 10};
+        int[] y = new int[] {2, 3, 9};
+        merge(x, y);
+        System.out.println(toList(x));
+        System.out.println(toList(y));
+    }
+}
